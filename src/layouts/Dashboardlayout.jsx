edcd34211fd7e1621ccf48ef64df42724df73bca
taki <takibou532@gmail.com>
2026-08-01 13:42:12 +0100

    fixing the importb for build

--- src/layouts/Dashboardlayout.jsx
+++ src/layouts/Dashboardlayout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router";
-import Sidebar from "../components/Sidebar";
+import Sidebar from "../components/sidebar";
 import Topbar from "../components/Topbar";
 import { SchoolProvider } from "../context/SchoolContext";
 
